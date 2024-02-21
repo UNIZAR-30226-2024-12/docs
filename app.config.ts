5793fa89f4b3719a5bc09f3af551dab8a1fc11f5
Andres <andreees.ys@gmail.com>
2024-02-21 22:16:53 +0100

    Title update

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'WealthWars Docs',
+    description: 'La documentación oficial de WealthWars.',
+    /*image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
@@ -18,7 +18,7 @@ export default defineAppConfig({
       repo: 'docus',
       owner: 'nuxt-themes',
       edit: true
-    },
+    },*/
     aside: {
       level: 0,
       collapsed: false,
@@ -29,8 +29,7 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
-      showLinkIcon: true,
+      title: 'WealthWars Docs',
       exclude: [],
       fluid: true
     }
